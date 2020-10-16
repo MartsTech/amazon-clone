eec6fd7dd0be157c9bcd2198803803180cd9b8c6
MartsTech <72291444+MartsTech@users.noreply.github.com>
2020-10-16 20:56:51 +0300

    added Header.js, Header.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     // BEM
     <div className="app">
       <h1>Let's do this!!</h1>
+      {/* Header */}
+      <Header />
+      {/* Home */}
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <img
+        className="header__logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+      />
+      <div className="header__search">
+        <input className="header__searchInput" type="text" />
+        {/* Logo */}
+      </div>
+
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello Guest</span>
+          <span className="header__optionLineTwo">Sign In</span>
+        </div>
+
+        <div className="header__option">
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwo">& Orders</span>
+        </div>
+
+        <div className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
