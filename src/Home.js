c69676aedf8f9ff07bbc8604b43372e13251b31e
MartsTech <72291444+MartsTech@users.noreply.github.com>
2020-10-17 13:51:51 +0300

    finished Home page version 1.0

--- src/Home.js
+++ src/Home.js
@@ -12,19 +12,55 @@ function Home() {
           alt=""
         />
         <div className="home__row">
-          {/* Product */}
-          <Product />
-          <Product />
-          {/* Product */}
+          <Product
+            id="49538094"
+            title='Apple 16" MacBook Pro with Touch Bar, 9th-Gen 8-Core Intel i9 2.3GHz'
+            price={3299.0}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/61UOtcMWy5L._AC_SX522_.jpg"
+          />
+          <Product
+            id="325435454"
+            title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
+            price={598.99}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
+          />
         </div>
 
         <div className="home__row">
-          {/* Product */}
-          {/* Product */}
-          {/* Product */}
+          <Product
+            id="234545430"
+            title="New Apple iPhone 12 Pro Max (128GB, Pacific Blue)"
+            price={1099.0}
+            image="https://m.media-amazon.com/images/I/71MHTD3uL4L.jpg"
+            rating={5}
+          />
+          <Product
+            id="23445930"
+            title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
+            price={98.99}
+            rating={5}
+            image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
+          />
+          <Product
+            id="4903850"
+            title="Samsung Galaxy Buds Live, True Wireless Earbuds"
+            price={169.99}
+            rating={3}
+            image="https://images-na.ssl-images-amazon.com/images/I/71LcAql4%2BaL._AC_SX679_.jpg"
+          />
         </div>
 
-        <div className="home__row">{/* Product */}</div>
+        <div className="home__row">
+          <Product
+            id="90829332"
+            title="Samsung 49-Inch CHG90 144Hz Curved Gaming Monitor"
+            price={1094.98}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/81Zt42ioCgL._AC_SX679_.jpg"
+          />
+        </div>
       </div>
     </div>
   );
--- src/Product.js
+++ src/Product.js
@@ -1,22 +1,26 @@
 import React from "react";
 import "./Product.css";
 
-function Product() {
+function Product({ title, image, price, rating }) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>The lean startup</p>
+        <p>{title}</p>
         <p className="product__price">
           <small>$</small>
-          <strong>19.99</strong>
+          <strong>{price}</strong>
         </p>
         <div className="product__rating">
+          {Array(rating)
+          .fill()
+          .map((_, i) =>(
           <p>⭐</p>
+          ))}
         </div>
       </div>
 
       <img
-        src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+        src={image}
         alt=""
       />
 
