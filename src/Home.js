71f256bcae3765efa632372fdd03ec8c93142d42
MartsTech <72291444+MartsTech@users.noreply.github.com>
2020-10-17 09:22:13 +0300

    added Product.js, Product.css

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Home.css";
+import Product from "./Product";
 
 function Home() {
   return (
@@ -10,8 +11,20 @@ function Home() {
           src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
           alt=""
         />
-        
-        
+        <div className="home__row">
+          {/* Product */}
+          <Product />
+          <Product />
+          {/* Product */}
+        </div>
+
+        <div className="home__row">
+          {/* Product */}
+          {/* Product */}
+          {/* Product */}
+        </div>
+
+        <div className="home__row">{/* Product */}</div>
       </div>
     </div>
   );
---
+++ src/Product.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Product.css";
+
+function Product() {
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>The lean startup</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>19.99</strong>
+        </p>
+        <div className="product__rating">
+          <p>⭐</p>
+        </div>
+      </div>
+
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+        alt=""
+      />
+
+      <button>Add to Basket</button>
+    </div>
+  );
+}
+
+export default Product;
